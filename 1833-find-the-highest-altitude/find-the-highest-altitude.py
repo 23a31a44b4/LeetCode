ad8f981eed69f3a65895cd0f506e27c461534ebe
23a31a44b4 <23a31a44b4@gmail.com>
2026-07-02 17:58:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (89.07%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def largestAltitude(self, gain):
+        ans=[0]
+        for i in range(len(gain)):
+            ans.append(ans[i]+gain[i])
+        return max(ans)
+
+        
