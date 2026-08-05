320b9d41e9632315e3dcc94d876f619ab4804c82
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-08-05 18:02:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (66.26%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,15 +1,11 @@
 class Solution(object):
     def intersection(self, nums1, nums2):
-        hash_map1={}
-        hash_map2={}
+        nums1=set(nums1)
+        nums2=set(nums2)
         ans=[]
-        for num in nums1:
-            hash_map1[num]=hash_map1.get(num,0)+1
-        for num in nums2:
-            hash_map2[num]=hash_map2.get(num,0)+1
-        for num in hash_map1:
-            if num in hash_map2:
-                ans.append(num)
+        for i in nums1:
+            if i in nums2:
+                ans.append(i)
         return ans
 
         
