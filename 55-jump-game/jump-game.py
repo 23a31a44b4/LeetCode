d00209428f3df3521fda01df56d67846e133b25c
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-08-06 15:05:53 +0530

    Time: 11 ms (91.55%) | Memory: 20.2 MB (84.74%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,8 +1,7 @@
-class Solution(object):
-    def canJump(self, nums):
-        goal=len(nums)-1
-        for i in range(len(nums)-1,-1,-1):
-            if i+nums[i]>=goal:
-                goal=i
-        return True if goal==0 else False
-        
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        des=len(nums)-1
+        for i in range(des-1,-1,-1):
+            if i+nums[i]>=des:
+                des = i
+        return des==0
