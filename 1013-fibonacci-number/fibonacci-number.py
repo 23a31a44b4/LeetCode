f2ac8e8bd59b478bef967a16debee59d65c41632
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-08-12 22:00:36 +0530

    Time: 16 ms (66.62%) | Memory: 12.3 MB (53.76%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def fib(self, n):
+        # def fib(n):
+        #     if n<2:
+        #         return n
+        #     return fib(n-1)+fib(n-2)
+        # return fib(n)
+
+        #memo
+        # memo = [-1]*(n+1)
+        # def memoization(n):
+        #     if n<2:
+        #         return n
+        #     if memo[n]!=-1:
+        #         return memo[n]
+        #     memo[n] = memoization(n-1)+memoization(n-2)
+        #     return memo[n]
+        # memoization(n)
+        # return memo[-1]      
+
+        #tabulation
+        # dp = [0]*(n+1)
+        # dp[1] = 1
+        # for i in range(2,n+1):
+        #     dp[i]=dp[i-1]+dp[i-2]
+        # return dp[-1]  
+
+        #spaceoptimization
+        if n<2:
+            return n
+        f0 = 0
+        f1 = 1
+        for i in range(2,n+1):
+            res = f0 + f1
+            f0 = f1
+            f1 = res
+        return f1
