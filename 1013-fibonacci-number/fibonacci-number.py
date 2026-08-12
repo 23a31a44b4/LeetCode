c43364dd369a602a72947c865fee9c254ea6aade
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-08-12 22:12:36 +0530

    Time: 9 ms (93.84%) | Memory: 12.4 MB (16.50%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -28,12 +28,19 @@ class Solution(object):
         # return dp[-1]  
 
         #spaceoptimization
-        if n<2:
+        # if n<2:
+        #     return n
+        # f0 = 0
+        # f1 = 1
+        # for _ in range(2,n+1):
+        #     res = f0 + f1
+        #     f0 = f1
+        #     f1 = res
+        # return res
+
+        if n <= 1:
             return n
-        f0 = 0
-        f1 = 1
-        for _ in range(2,n+1):
-            res = f0 + f1
-            f0 = f1
-            f1 = res
-        return res
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
