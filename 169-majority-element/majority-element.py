918819e564f7ced43312f257f0ebad1a533de3ad
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-08-17 14:17:48 +0530

    Time: 14 ms (31.08%) | Memory: 13.6 MB (39.39%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,21 @@
 class Solution(object):
     def majorityElement(self, nums):
-        candidate=nums[0]
-        count=1
-        for i in nums[1:]:
-            if i==candidate:
-                count+=1
-            elif i!=candidate:
-                if count==0:
-                    candidate=i
-                else:
-                    count-=1
-        return candidate
+        # candidate=nums[0]
+        # count=1
+        # for i in nums[1:]:
+        #     if i==candidate:
+        #         count+=1
+        #     elif i!=candidate:
+        #         if count==0:
+        #             candidate=i
+        #         else:
+        #             count-=1
+        # return candidate
+        n=len(nums)
+        hash_map={}
+        for i in nums:
+            hash_map[i]=hash_map.get(i,0)+1
+        for k,v in hash_map.items():
+            if v>(n/2):
+                return k
         
