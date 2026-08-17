1be0bd17de7f1dc86e33b59d5f3bbd3564a286b5
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-08-17 14:27:36 +0530

    Time: 6 ms (57.25%) | Memory: 12.9 MB (13.31%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,20 @@
 class Solution(object):
     def twoSum(self, numbers, target):
-        hash_map={}
-        for i in range(len(numbers)):
-            if  numbers[i] in hash_map:
-                return [hash_map[numbers[i]]+1,i+1]
-            hash_map[target-numbers[i]]=i
+        # hash_map={}
+        # for i in range(len(numbers)):
+        #     if  numbers[i] in hash_map:
+        #         return [hash_map[numbers[i]]+1,i+1]
+        #     hash_map[target-numbers[i]]=i
+
+        l=0
+        r=len(numbers)-1
+        while l<r:
+            mid=numbers[l]+numbers[r]
+            if target==mid:
+                return [l+1,r+1]
+            elif target<=mid:
+                r-=1
+            else:
+                l+=1
         
         
