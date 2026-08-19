db69992bb354264ec0eb8f4d5fdedcb2b10ac524
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-08-19 23:54:19 +0530

    Time: 920 ms (5.10%) | Memory: 22.5 MB (21.97%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,26 +1,39 @@
 class Solution(object):
     def productExceptSelf(self, nums):
-        ans=1
-        zero=0
+        prefix=[1]
+        postfix=[1]*(len(nums)+1)
+        res=[]
         for num in nums:
-            if num==0:
-                zero+=1
-            else:
-                ans*=num
-        print(ans)
+            prefix.append(prefix[-1]*num)
+        print(prefix)
+        for i in range(len(nums)-1,-1,-1):
+            postfix[i]=postfix[i+1]*nums[i]
         
-        if zero==0:
-            res=[]
-            for num in nums:
-                res.append(ans//num)
-            
-        elif zero==1:
-            res=[]
-            for num in nums:
-                if num==0:
-                    res.append(ans)
-                else:
-                    res.append(0)
-        elif zero>=2:
-            res=[0]*len(nums)
+        print(postfix)
+        for i in range(len(nums)):
+            res.append(prefix[i]*postfix[i+1])
         return res
+        # ans=1
+        # zero=0
+        # for num in nums:
+        #     if num==0:
+        #         zero+=1
+        #     else:
+        #         ans*=num
+        # print(ans)
+        
+        # if zero==0:
+        #     res=[]
+        #     for num in nums:
+        #         res.append(ans//num)
+            
+        # elif zero==1:
+        #     res=[]
+        #     for num in nums:
+        #         if num==0:
+        #             res.append(ans)
+        #         else:
+        #             res.append(0)
+        # elif zero>=2:
+        #     res=[0]*len(nums)
+        # return res
