8d4b9b472e7f25357a0916c574c36830b18944b8
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-08-20 22:24:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,13 +1,25 @@
-class Solution(object):
-    def isValid(self, s):
+class Solution:
+    def isValid(self, s: str) -> bool:
         stack=[]
+        if s[0]==')' or s[0]==']' or s[0]=='}':
+            return False
         for ch in s:
-            if ch=='(':
-                stack.append(')')
-            elif ch=='[':
-                stack.append(']')
-            elif ch=='{':
-                stack.append('}')
-            elif not stack or ch!=stack.pop():
-                return False
-        return not stack
+            if ch=='(' or ch=='[' or ch =='{':
+                stack.append(ch)
+                continue
+            elif ch ==')':
+                if stack and stack[-1]=='(':
+                    stack.pop()
+                else:
+                    stack.append(ch)
+            elif ch == ']':
+                if stack and stack[-1]=='[':
+                    stack.pop()
+                else:
+                    stack.append(ch)
+            elif ch =='}' :
+                if stack and stack[-1]=='{':
+                    stack.pop()
+                else:
+                    stack.append(ch)
+        return len(stack)==0
