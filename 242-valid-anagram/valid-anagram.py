ebf99c0d98cdbdf0974cf314086740b4c8766054
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-08-20 14:46:47 +0530

    Time: 11 ms (77.36%) | Memory: 19.3 MB (76.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        heap_map_s={}
-        heap_map_t={}
-        for i in range(len(s)):
-            heap_map_s[s[i]]=heap_map_s.get(s[i],0)+1
-            heap_map_t[t[i]]=heap_map_t.get(t[i],0)+1
-        return heap_map_s==heap_map_t
+       hash_s={}
+       hash_t={}
+       if len(s)!=len(t):
+        return False
+       for i in range(len(s)):
+        hash_s[s[i]]=hash_s.get(s[i],0)+1
+        hash_t[t[i]]=hash_t.get(t[i],0)+1
+       return hash_t==hash_s
