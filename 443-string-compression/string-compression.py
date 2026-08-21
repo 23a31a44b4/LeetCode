4de23b678feac3441cded7bc2ec6179851388e5b
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-08-21 18:03:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.43%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,31 +1,19 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        # hash_map={}
-        # for ch in chars:
-        #     hash_map[ch]=hash_map.get(ch,0)+1
-        # res=[]
-        # for k,v in hash_map.items():
-        #     res.append(k)
-        #     if v!=1:
-        #         for digit in str(v):
-        #             res.append(digit)
-                
-        # chars[:]=res
-        # return len(chars)
-        s=''
+        write=0
         n=len(chars)
         i=0
         while i<n:
+            char=chars[i]
             count=1
             while i+1<n and chars[i]==chars[i+1]:
                 count+=1
                 i+=1
-            s+=chars[i]
+            chars[write]=char
+            write+=1
             if count>1:
-                s+=str(count)
+                for digit in str(count):
+                    chars[write]=digit
+                    write+=1
             i+=1
-        c_len=len(s)
-        for j in range(c_len):
-            chars[j]=s[j]
-        del chars[c_len:]
-        return len(s)
+        return write
