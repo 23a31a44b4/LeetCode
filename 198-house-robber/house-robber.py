af8dc6c99ab874a3e34044c366fa329d408760d3
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-09-11 14:52:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (90.42%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -6,12 +6,19 @@ class Solution:
         #     return max(nums[n]+backtrack(n+2),backtrack(n+1))
         # return backtrack(0)
 
-        def memoization(n):
-            if n>=len(nums):
-                return 0
-            if memo[n]!=-1:
-                return memo[n]
-            memo[n]=max(nums[n]+memoization(n+2),memoization(n+1))
-            return memo[n]
-        memo=[-1]*len(nums)
-        return memoization(0)
+        # def memoization(n):
+        #     if n>=len(nums):
+        #         return 0
+        #     if memo[n]!=-1:
+        #         return memo[n]
+        #     memo[n]=max(nums[n]+memoization(n+2),memoization(n+1))
+        #     return memo[n]
+        # memo=[-1]*len(nums)
+        # return memoization(0)
+
+        dp=[-1]*(len(nums))
+        dp.append(0)
+        dp.append(0)
+        for i in range(len(nums)-1,-1,-1):
+            dp[i]=max(nums[i]+dp[i+2],dp[i+1])
+        return dp[0]
