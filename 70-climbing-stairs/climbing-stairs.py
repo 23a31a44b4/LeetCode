420d7c80218dde1c1fffc95f8e6dd11b1eb10d74
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-09-11 13:52:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.42%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,19 +1,11 @@
-class Solution(object):
-    def climbStairs(self, n):
-        # memo=[-1]*(n+1)
-        # def backtrack(n):
-        #     if n<=2:
-        #         return n
-        #     if memo[n]!=-1:
-        #         return memo[n]
-        #     memo[n]=backtrack(n-1)+backtrack(n-2)
-        #     return memo[n]
-        # return backtrack(n)
-        if n<=2:
-            return n
-        dp = [0]*n
-        dp[0] = 1
-        dp[1] = 2
-        for i in range(2,n):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[-1]
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def memoization(n):
+            if n<=2:
+                return n
+            if memo[n]!=-1:
+                return memo[n]
+            memo[n]=memoization(n-1)+memoization(n-2)
+            return memo[n]
+        memo=[-1]*(n+1)
+        return memoization(n)
