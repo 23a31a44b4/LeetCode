cb9132566e3efa41c1b86ccc13fb5db1b8616673
Bandaru Sai Chandu <23a31a44b4@gmail.com>
2026-09-11 14:57:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (98.68%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,5 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        r1=0
+        r2=0
+        for i in range(len(nums)-1,-1,-1):
+            r=max(nums[i]+r2,r1)
+            r2=r1
+            r1=r
+        return r1
         # def backtrack(n):
         #     if n>=len(nums):
         #         return 0
@@ -16,9 +23,7 @@ class Solution:
         # memo=[-1]*len(nums)
         # return memoization(0)
 
-        dp=[-1]*(len(nums))
-        dp.append(0)
-        dp.append(0)
-        for i in range(len(nums)-1,-1,-1):
-            dp[i]=max(nums[i]+dp[i+2],dp[i+1])
-        return dp[0]
+        # dp=[-1]*len(nums)+[0,0]
+        # for i in range(len(nums)-1,-1,-1):
+        #     dp[i]=max(nums[i]+dp[i+2],dp[i+1])
+        # return dp[0]
